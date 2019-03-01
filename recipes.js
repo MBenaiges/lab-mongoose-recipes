3ce4e24327d35eb45e608a4987d2cac9e3831d35
Marc Benaiges <benaiges.marc@gmail.com>
2019-03-01 14:03:52 +0100

    done

--- recipes.js
+++ recipes.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 const data = require('./data.js');
+const fs = require('fs');
+
+const Recipe = require('./Recipe');
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -8,3 +11,38 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+// --- CREAR ---
+// Recipe.create({ title: 'Paella', cuisine: 'Spanish' })
+// .then(result => {
+//   console.log(result);
+//   mongoose.connection.close();
+// })
+// .catch(err => console.log(err));
+
+// --- INSERT ALL RECIPES ---
+// Recipe.insertMany(data)
+//   .then(result => {
+//     console.log(result);
+//     mongoose.connection.close();
+//   })
+//   .catch(err => console.log(err));
+
+// --- UPDATE ---
+// Recipe.update({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, { new: true })
+//   .then(result => {
+//     console.log(result);
+//     console.log('succes!');
+//     mongoose.connection.close();
+//   })
+//   .catch(err => console.log(err));
+
+// --- REMOVE ---
+Recipe.deleteOne({ title: 'Carrot Cake' })
+  .then(result => {
+    console.log(result);
+    mongoose.connection.close();
+  })
+  .catch(err => console.log(err));
+
+mongoose.connection.close();
